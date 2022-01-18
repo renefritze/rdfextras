44ccf3ba1ccfc0e0c434146b98a5e236021da56d
René Fritze <rene.fritze@wwu.de>
2022-01-18 09:00:18 +0100

    get rid of python2 compat in setup.py

--- setup.py
+++ setup.py
@@ -3,32 +3,6 @@
 import sys
 import re
 
-def setup_python3():
-    # Taken from "distribute" setup.py
-    from distutils.filelist import FileList
-    from distutils import dir_util, file_util, util, log
-    from os.path import join
-  
-    tmp_src = join("build", "src")
-    log.set_verbosity(1)
-    fl = FileList()
-    for line in open("MANIFEST.in"):
-        if not line.strip():
-            continue
-        fl.process_template_line(line)
-    dir_util.create_tree(tmp_src, fl.files)
-    outfiles_2to3 = []
-    for f in fl.files:
-        outf, copied = file_util.copy_file(f, join(tmp_src, f), update=1)
-        if copied and outf.endswith(".py"):
-            outfiles_2to3.append(outf)
-  
-    util.run_2to3(outfiles_2to3)
-  
-    # arrange setup to use the copy
-    sys.path.insert(0, tmp_src)
-
-    return tmp_src
 
 # Find version. We have to do this because we can't import it in Python 3 until
 # its been automatically converted in the setup process.
@@ -64,18 +38,8 @@ config = dict(
                 'rdfextras.utils']
 )
 
-if sys.version_info[0] >= 3:
-    from setuptools import setup
-    config.update({'use_2to3': True})
-    config.update({'src_root': setup_python3()})
-    install_requires = ['rdflib >= 3.2.1', 'pyparsing']
-else:
-    try:
-        from setuptools import setup
-        config.update({'test_suite' : "nose.collector"})
-    except ImportError:
-        from distutils.core import setup
-    install_requires = ['rdflib >= 3.2.1', 'pyparsing<=1.5.7']
+from setuptools import setup
+install_requires = ['rdflib >= 3.2.1', 'pyparsing']
 
 
 tests_require = install_requires + \
